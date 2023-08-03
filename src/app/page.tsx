1e75d28af8a4d349710019bc57ec7c84315b0bab
Aimee Tonks <aimztonks@gmail.com>
2023-08-03 16:26:23 +0100

    front page work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,32 @@
 import Image from 'next/image'
 import React from 'react'
-import Navbar from './navbar'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div id='title'>
-        <h1>CV Site</h1>
+      <section>
+        <video autoPlay muted loop className='intro'>         
+          <source src="/introVid.mp4" type="video/mp4"/>       
+        </video>
+      </section>
+      <div>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+        <h1>this is just some filler so that i can see if the video and footer move</h1>
+
       </div>
+      <footer>
+        <p><b>Find me on other platforms:</b></p>
+        <ul>
+          <li>LinkedIn</li>
+          <li>GitHub</li>
+        </ul>
+      </footer>
     </div>
   )
 }
