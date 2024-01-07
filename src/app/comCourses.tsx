de0b5ab0831595cd804c7e4566b5ee43dbb75f0d
Aimee Tonks <aimztonks@gmail.com>
2024-01-07 15:55:08 +0000

    Conferences

--- src/app/comCourses.tsx
+++ src/app/comCourses.tsx
@@ -3,14 +3,23 @@ import React from 'react'
 export default function Courses() {
   return (
     <div className='fill'>
-        <h1>Relevant Courses</h1>
-        <small>Here are some courses that I have completed during my breaks from university</small>
+        <h1>Extracurricular Courses and Excursions</h1>
+        <small>Here are some courses and events that I have completed during my breaks from university</small>
         <div className='year'>
             <h2>Courses from Scrimba:</h2>
             <ul>
                 <li>Learn HTML and CSS - <a href='https://scrimba.com/learn/htmlandcss'>(Click Here)</a></li>
                 <li>Learn JavaScript - <a href='https://scrimba.com/learn/learnjavascript'>(Click Here)</a></li>
                 <li>Learn React - <a href='https://scrimba.com/learn/learnreact'>(Click Here)</a></li>
+            </ul>
+        </div>
+        <div className='year'>
+            <h2>Excursions:</h2>
+            <ul>
+              <li>Attended the <a href='https://aws.amazon.com/events/summits/london/'>AWS Summit 2023</a>
+               at the ExCeL Centre, London</li>
+              <li>Attended the <a href='https://www.manchestertechfestival.co.uk/'>Manchester Tech Festival 2023</a>
+               at Victoria Baths, Manchester</li>
             </ul>
         </div>
     </div>
--- src/app/intro.tsx
+++ src/app/intro.tsx
@@ -5,11 +5,11 @@ export default function Intro() {
     <div className='fill'>
         <h1>Get to know me</h1>
         <div className='year'>
-            <p>Currently, I&apos;m in my third year at Keele University doing my Computer Science (Software
-            Engineering) degree and I&apos;m due to graduate in the summer of 2024. I am hoping to recieve
-            either a 2:1 or a 1st and have been putting my all into my degree because of this.</p><br/>
+            <p>Currently, I&apos;m in my third year at Keele University completing my degree in BSc Computer Science (Software
+            Engineering) and I&apos;m due to graduate in the summer of 2024. I am hoping to recieve
+            either a 2:1 or a 1st and have been putting my all into my degree to ensure this.</p><br/>
             <p>Outside of learning, I work in the kitchen at the Student&apos;s Union to gain a bit of 
-              extra money alongside my studies as I don&apos;t have any student finance left after paying rent.
+              extra money alongside my studies, and also enjoy my more creative hobbies like drawing and reading.
             </p>
         </div>
     </div>
